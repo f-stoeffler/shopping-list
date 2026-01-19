422e84f3c8628385b22fcfea21ed5be1378f850c
flottschi <florian.stoeffler@edu.htl-villach.at>
2026-01-19 14:18:40 +0100

    add exception listener for api route

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+final class ExceptionListener
+{
+    #[AsEventListener]
+    public function onExceptionEvent(ExceptionEvent $event): void
+    {
+        $request = $event->getRequest();
+        $path = $request->getPathInfo();
+        if (!str_starts_with($path, "/api")) {
+            return;
+        }
+
+        $exception = $event->getThrowable();
+        $response = new JsonResponse([
+            "error" => $exception->getMessage()
+        ]);
+        $event->setResponse($response);
+    }
+}
